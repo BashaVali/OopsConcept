7d04d62d243834b016f45e2c68c33e206c4f5424
BashaVali <bashavali222@gmail.com>
2022-11-14 06:48:26 +0530

    Inventory Data Management

---
+++ Oops/Program.cs
@@ -0,0 +1,14 @@
+﻿using Oops.InventoryManagement;
+using System;
+namespace OOPSPrograms
+{
+    class Program
+    {
+        static string InventoryFilePath = @"C:\Users\Basha\Documents\DotNet\PracticePrograms\OopsConcept\Oops\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
+            inventoryDetailsManagement.ReadJSonFile(InventoryFilePath);
+        }
+    }
+}
